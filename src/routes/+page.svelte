0d5f08c87292504cc9747c7a35e7c53490ac855c
NotToxic <47497223+NotToxicDev@users.noreply.github.com>
2023-01-31 18:03:26 +0100

    basic styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="page">
+    <h1>Hello, World!</h1>
+    
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem itaque nihil ad cupiditate dolorum tenetur alias error, eum nostrum soluta totam eius explicabo iste vel nam voluptatum incidunt eveniet veniam!</p>
+</div>
+
+<style lang="scss">
+    .page {
+        padding: 1em;
+        margin: 2.5%;
+        height: 100%;
+        width: 100%;
+        border-radius: 10px;
+        background-color: #373736;
+    }
+</style>
