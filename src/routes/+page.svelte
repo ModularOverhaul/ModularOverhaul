7448c8f89bab0a507c313bb659ba8ec89fee852f
NotToxic <47497223+NotToxicDev@users.noreply.github.com>
2023-01-31 19:12:33 +0100

    infobar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,23 @@
+<script lang="ts">
+    const ver = "v0.0.1-alpha"
+    const dsc = "https://discord.gg/vS4zQ8PvPW"
+    const gh = "https://github.com/modularoverhaul"
+    const twt = "https://twitter.com/ModularOverhaul"
+</script>
+
 <div class="page">
+    <div class="info">
+        <p>
+            <a href="{dsc}">
+                <i class="fa-brands fa-discord"/>
+            </a> 
+            <a href="{gh}">
+                <i class="fa-brands fa-github"/>
+            </a> <a href="{twt}">
+                <i class="fa-brands fa-twitter"/>
+            </a> {ver}
+        </p>
+    </div>
     <h1>Hello, World!</h1>
     
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem itaque nihil ad cupiditate dolorum tenetur alias error, eum nostrum soluta totam eius explicabo iste vel nam voluptatum incidunt eveniet veniam!</p>
@@ -12,5 +31,9 @@
         width: 100%;
         border-radius: 10px;
         background-color: #373736;
+    }
+    .info {
+        float: right;
+        p {margin: 0;}
     }
 </style>
